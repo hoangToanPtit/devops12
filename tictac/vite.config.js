37636c3a287c05226f705d92584582a828926926
toannv <toannv@bsscommerce.com>
2023-05-29 20:12:59 +0700

    add tictac

--- tictac/vite.config.js
+++ tictac/vite.config.js
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: "0.0.0.0"
-  },
   plugins: [react()],
   base: "/tictac/",
 });
